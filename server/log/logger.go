15a47d79dce2fc1165c1bc72a8fcbb571300574d
lian <msn19882009@live.cn>
2025-10-24 08:20:56 +0800

    feat: [CS-3] output log to console for promtail to collect (#12)

--- server/log/logger.go
+++ server/log/logger.go
@@ -18,7 +18,9 @@ var (
 func InitLogger() {
 	writeSyncer := getLogWriter()
 	encoder := getEncoder()
-	core := zapcore.NewCore(encoder, writeSyncer, getLogLevel())
+	fileCore := zapcore.NewCore(encoder, writeSyncer, getLogLevel())
+	consoleCore := zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), getLogLevel())
+	core := zapcore.NewTee(fileCore, consoleCore)
 	Logger = zap.New(core, zap.AddCaller()).Sugar()
 }
 
